07554ca269eaea4b0f98f44320d9ca313454b237
Sachin Bhoi\ASUS <givesachin@gmail.com>
2023-01-21 17:43:02 +0530

    dark light text selector

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ app.controller('ncaaController', function($scope, $http) {
             });
     };
 
+    $scope.getColor = function()
+    {
+        return '#000000';
+    }
+
     $scope.init();
 
+});
+
+app.filter('getColor', function() {
+    return function(hex) {
+
+        const r = parseInt(hex.slice(1, 3), 16)
+        const g = parseInt(hex.slice(3, 5), 16)
+        const b = parseInt(hex.slice(5, 7), 16)
+
+        return (((r * 299) + (g * 587) + (b * 114)) / 1000) >= 128 ? '000' : 'fff';;
+    };
 });
