43c657040d6d6e819114f2a286ba6d6e27289eac
Sachin Bhoi\ASUS <givesachin@gmail.com>
2023-01-23 14:20:50 +0530

    pagination

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ app.controller('ncaaController', function($scope, $http) {
                 });
 
                 $scope.data = response.data.Output;
+                $scope.paginateData(1);
 
                 $("#overlay").fadeOut();
             })
@@ -29,6 +30,147 @@ app.controller('ncaaController', function($scope, $http) {
     {
         return '#000000';
     }
+
+
+    $scope.paginated_data = [];
+    
+    $scope.pagination_details = {};
+    $scope.per_page_filter = 25;
+    $scope.currentPage = 1;
+    $scope.startPage = 0;
+    $scope.endPage = 0;
+    $scope.page_size = [10, 25, 50, 100, 250, 500];
+    
+    $scope.paginateData = function (page_number = null)
+    {
+        if (page_number != null)
+        {
+            $scope.pagination_details.current_page = page_number;
+        }
+
+        //START - Stack Pointers and Pagination Details updater
+        if ($scope.data.length <= $scope.per_page_filter)
+        {
+            //IF there is single page
+            $scope.pagination_details.last_page = 1;
+            $scope.pagination_details.from = 1;
+            $scope.pagination_details.to = $scope.data.length;
+        } else
+        {
+            //IF there are multiple pages
+            var remainder = $scope.data.length % $scope.per_page_filter;
+
+            if (remainder === 0)
+            {
+                //IF last page is a complete, last page size is equal to per page size
+                $scope.pagination_details.last_page = parseInt(($scope.data.length / $scope.per_page_filter));
+
+                //SET index for any of all complete pages
+                $scope.pagination_details.to = $scope.per_page_filter * $scope.pagination_details.current_page;
+                $scope.pagination_details.from = $scope.pagination_details.to - $scope.per_page_filter + 1;
+            } else
+            {
+                //IF last page is not a complete, last page size is less than per page size
+                $scope.pagination_details.last_page = parseInt(($scope.data.length / $scope.per_page_filter)) + 1;
+
+                if ($scope.pagination_details.current_page < $scope.pagination_details.last_page)
+                {
+                    //SET index for any of complete pages except last page
+                    $scope.pagination_details.to = $scope.per_page_filter * $scope.pagination_details.current_page;
+                    $scope.pagination_details.from = $scope.pagination_details.to - $scope.per_page_filter + 1;
+                } else
+                {
+                    //SET index for last page
+                    $scope.pagination_details.to = ($scope.per_page_filter * ($scope.pagination_details.current_page - 1)) + remainder;
+                    $scope.pagination_details.from = $scope.pagination_details.to - remainder + 1;
+                }
+            }
+        }
+        //END - Stack Pointers and Pagination Details updater
+
+        //SET dropdown for all pages
+        $scope.all_pages = [];
+
+        for (var i = 1; i <= $scope.pagination_details.last_page; i++)
+        {
+            $scope.all_pages.push(i);
+        }
+
+        //SET marked or paginated array to the table
+        $scope.paginated_data = $scope.data.slice($scope.pagination_details.from - 1, $scope.pagination_details.to);
+console.log($scope.paginated_data);
+        //START - Pagination Links for medium size screen
+        $scope.pages = [];
+
+        if ($scope.pagination_details.last_page <= 5)
+        {
+            // less than 10 total pages so show all
+            $scope.startPage = 1;
+            $scope.endPage = $scope.pagination_details.last_page;
+        } else
+        {
+            // more than 10 total pages so calculate start and end pages
+            if ($scope.pagination_details.current_page <= 3)
+            {
+                $scope.startPage = 1;
+                $scope.endPage = 5;
+            } else if ($scope.pagination_details.current_page + 2 >= $scope.pagination_details.last_page)
+            {
+                $scope.startPage = $scope.pagination_details.last_page - 4;
+                $scope.endPage = $scope.pagination_details.last_page;
+            } else
+            {
+                $scope.startPage = $scope.pagination_details.current_page - 2;
+                $scope.endPage = $scope.pagination_details.current_page + 2;
+            }
+        }
+
+        for (var i = $scope.startPage; i <= $scope.endPage; i++)
+        {
+            $scope.pages.push(i);
+        }
+        //END - Pagination Links for medium size screen
+
+        //START - Pagination Links for large size screen
+        $scope.pages_more = [];
+
+        if ($scope.pagination_details.last_page <= 9)
+        {
+            // less than 10 total pages so show all
+            startPage_more = 1;
+            endPage_more = $scope.pagination_details.last_page;
+        } else
+        {
+            // more than 10 total pages so calculate start and end pages
+            if ($scope.pagination_details.current_page <= 5)
+            {
+                startPage_more = 1;
+                endPage_more = 9;
+            } else if ($scope.pagination_details.current_page + 4 >= $scope.pagination_details.last_page)
+            {
+                startPage_more = $scope.pagination_details.last_page - 8;
+                endPage_more = $scope.pagination_details.last_page;
+            } else
+            {
+                startPage_more = $scope.pagination_details.current_page - 4;
+                endPage_more = $scope.pagination_details.current_page + 4;
+            }
+        }
+
+        for (var i = startPage_more; i <= endPage_more; i++)
+        {
+            $scope.pages_more.push(i);
+        }
+        //END - Pagination Links for large size screen
+        $scope.setDropdownSelection();
+    };
+
+    $scope.calcCursorPage = function (divisor)
+    {
+        var dividend = $scope.pagination_details.from - 1;
+
+        $scope.paginateData(Math.floor(dividend / divisor) + 1);
+    };
 
     $scope.init();
 
